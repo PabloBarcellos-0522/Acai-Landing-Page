82532bae994c5bd46009195c283c6110d67c898b
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-05-09 21:17:39 -0300

    effects and descriptions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var img = document.getElementsByTagName("img")
+var header = document.getElementById("finalHeder")
+var list = document.getElementsByTagName("li")
 
 // a = setInterval(() => {
 //   if (img[0].getBoundingClientRect().top < window.innerHeight) {
@@ -8,12 +10,24 @@ var img = document.getElementsByTagName("img")
 // }, 100)
 
 function ativaNoScroll() {
-  if (img[0].getBoundingClientRect().top < window.innerHeight - 100) {
-    console.log("aaa")
+  if (img[0].getBoundingClientRect().top > header.getBoundingClientRect().top + 500) {
     img[0].src = img[0].getAttribute("data-src")
     document.getElementById("img").classList.add("viewed")
-    // clearInterval(a)
   }
+
+  if (img[0].getBoundingClientRect().top < header.getBoundingClientRect().top + 500) {
+    img[0].src = img[0].getAttribute("data-src")
+    document.getElementById("img").classList.remove("viewed")
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (img[0].getBoundingClientRect().top > list[i].getBoundingClientRect().top - 350) {
+      // list[0].style.opacity = "1"
+      console.log(img[0].getBoundingClientRect().top, list[i].getBoundingClientRect().top)
+      list[i].classList.add("active")
+    }
+  }
+  console.log(img[0].getBoundingClientRect().top)
 }
 
 window.addEventListener("scroll", ativaNoScroll)
